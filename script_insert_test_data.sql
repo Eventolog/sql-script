850d5de1b6d5285abe6a76cb7abe24edd34a1ab3
Von <148581386+rwxce@users.noreply.github.com>
2025-05-12 14:05:39 +0000

    hot fix

--- script_insert_test_data.sql
+++ script_insert_test_data.sql
@@ -47,11 +47,11 @@ INSERT INTO messages (content, status, sender_id, chat_id) VALUES
 ('Hey Charlie, how are you?', 'delivered', 2, 2),
 ('Meeting at 3 PM', 'read', 1, 3);
 
--- EVENTS (referenciem sales 1, 2 i 3 que ara existeixen)
+-- EVENTS
 INSERT INTO events (name, description, if_full_day, start_time, end_time, status, organizer_id, room_id) VALUES
-('Tech Conference', 'Annual technology conference', 0, '2025-06-01 09:00:00', '2025-06-01 18:00:00', 'scheduled', 2, 1),
-('Startup Pitch', 'Pitch your startup ideas', 0, '2025-06-10 10:00:00', '2025-06-10 13:00:00', 'scheduled', 2, 2),
-('Music Festival', 'Outdoor music festival', 1, '2025-07-15 00:00:00', '2025-07-15 23:59:00', 'scheduled', 2, 3);
+('Tech Conference', 'Annual technology conference', 0, CONVERT(DATETIME, '2025-06-01 09:00:00', 120), CONVERT(DATETIME, '2025-06-01 18:00:00', 120), 'scheduled', 2, 1),
+('Startup Pitch', 'Pitch your startup ideas', 0, CONVERT(DATETIME, '2025-06-10 10:00:00', 120), CONVERT(DATETIME, '2025-06-10 13:00:00', 120), 'scheduled', 2, 2),
+('Music Festival', 'Outdoor music festival', 1, CONVERT(DATETIME, '2025-07-15 00:00:00', 120), CONVERT(DATETIME, '2025-07-15 23:59:00', 120), 'scheduled', 2, 3);
 
 -- SEATS
 INSERT INTO seats (row_number, seat_number, room_id) VALUES
